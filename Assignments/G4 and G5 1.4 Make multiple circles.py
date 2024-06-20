d8adcabb3ce71a6845b41404c783c0b38c3ad816
AvaniAbhijit <avani.abhijit@orchids.edu.in>
2024-06-20 11:42:24 +0530

    Update G4 and G5 1.4 Make multiple circles.py

--- Assignments/G4 and G5 1.4 Make multiple circles.py
+++ Assignments/G4 and G5 1.4 Make multiple circles.py
@@ -4,7 +4,7 @@ t = turtle.Turtle()
 t.shape("turtle")
 t.pencolor("red")
 t.circle(50)
-t.circle(80)      # Creating another circle with a radius of 80
+                         # Creating another circle with a radius of 80
 
 
 turtle.done()# Write your code here :-)
